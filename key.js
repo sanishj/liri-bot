c018e7f6c4b428c4dfd465e784c7d5aa701bb6cb
sanishjohn <sanishj@gmail.com>
2017-07-22 08:23:49 -0500

    incremental changes

---
+++ key.js
@@ -0,0 +1,17 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '2RbVIbNeSOTU6EiBDTciOd141',
+  consumer_secret: 'kb7stJZ7fFbwUBGmc0Dk3oFSfTaMMJZvjVbXCqt6Qbswo3sxum',
+  access_token_key: '888544821551517697-sIf0yDSCFkxyzXuF3epOL82dNje2oNs',
+  access_token_secret: 'mqkS3htzWYSE3qX3mrtPuq4mRSByi6uUVo7RB0p061y08',
+};
+
+exports.omdbApiKey = {
+	apikey: '40e9cece'
+};
+
+exports.spotify = {
+	id: 'c783a9cffd344c80b15496157261e84f',
+	secret: '6a0312331bcb4d74b3ddc2942c29438e'
+};
--- liri.js
+++ liri.js
@@ -1,3 +1,43 @@
-var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
 var request = require('request');
+var fs = require('fs');
+var keys = require("./key.js");
+
+
+// Calling all API keys 
+var twitterKeys = keys.twitterKeys;
+var spotifyKeys = keys.spotify;
+var omdbApiKey = keys.omdbApiKey.apikey
+// Test api key calling
+console.log("OMDB: " + omdbApiKey + "\n-------------\n" + "Twitter: " + twitterKeys + "\n-------------\n" + "Spotify: " + spotifyKeys);
+
+/*var getMeMovie = function(movieName) {
+
+        if (movieName === undefined) {
+            movieName = 'Mr Nobody';
+        }
+
+        var movieURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json";
+
+        request(movieURL, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var movieData = [];
+                var jsonData = JSON.parse(body);
+
+                movieData.push({
+                    'Title: ': jsonData.Title,
+                    'Year: ': jsonData.Year,
+                    'Rated: ': jsonData.Rated,
+                    'IMDB Rating: ': jsonData.imdbRating,
+                    'Country: ': jsonData.Country,
+                    'Language: ': jsonData.Language,
+                    'Plot: ': jsonData.Plot,
+                    'Actors: ': jsonData.Actors,
+                    'Rotten Tomatoes Rating: ': jsonData.tomatoRating,
+                    'Rotton Tomatoes URL: ': jsonData.tomatoURL,
+                });
+                console.log(movieData);
+                writeToLog(movieData);
+            }
+        });*/
